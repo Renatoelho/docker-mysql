88146cab23c01d4fe8dcdbad5fdb8e2b4d49cef5
renatoelho <renatoelho@gmail.com>
2024-02-22 05:04:21 -0300

    Update repo 20240222043739...

---
+++ deploy/init/create.sql
@@ -0,0 +1,25 @@
+-- create databases
+create database if not exists db_system01;
+
+-- create users
+create user 'user_system01'@'%' identified by 'rQOIg0xRyoDS';
+
+-- create permission
+grant all privileges on db_system01.* to 'user_system01'@'%';
+
+-- Criar a tabela
+CREATE TABLE db_system01.custormes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    age INT,
+    city VARCHAR(100),
+    email VARCHAR(100),
+    datetime_stamp datetime DEFAULT CURRENT_TIMESTAMP
+);
+
+-- insert records
+INSERT INTO db_system01.custormes (nome, idade, cidade, email)
+VALUES
+('João', 30, 'São Paulo', 'joao@example.com'),
+('Maria', 25, 'Rio de Janeiro', 'maria@example.com'),
+('Pedro', 35, 'Belo Horizonte', 'pedro@example.com');
